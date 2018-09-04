61fdf0711106d9db198065d3cca71312f362b19d
Brett Shearin <brettcshearin@hotmail.com>
2018-09-04 03:02:49 -0500

    Numbers are randomized and squared

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<int> rndNums = new List<int>();
+
+            for(int i=0; i <20; i++)
+            {
+                rndNums.Add(random.Next(1,51));
+                Console.WriteLine($"Random Numbers - {rndNums[i]}");
+            }
+
+            List<int> sqrNums = new List<int>();
+            foreach (int num in rndNums)
+            {
+                sqrNums.Add(num * num);
+            }
+            sqrNums.RemoveAll(num => num % 2 == 0);
+            foreach (int item in sqrNums)
+            {
+                Console.WriteLine($"Squared number - {item}");
+            }
+        }
+    }
+}
